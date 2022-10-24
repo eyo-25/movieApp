2ad97c33c2999ccbd85d2ea1939cae4f80988cc4
eyo25 <qutuzm@naver.com>
2022-10-24 23:35:18 +0900

    #02 movieApp

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { Text, View, Image, useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import Root from "./navigation/Root";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./theme";
+
+SplashScreen.preventAutoHideAsync();
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const isDark = useColorScheme() === "dark";
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        //초기화 함수 넣으면됨
+        const fonts = loadFonts([Ionicons.font]);
+        const images = loadImages([
+          require("./ddabong.jpg"),
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png",
+        ]);
+        await Promise.all([...fonts, ...images]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <StatusBar style={isDark ? "light" : "dark"} />
+        <View onLayout={onLayoutRootView}></View>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
